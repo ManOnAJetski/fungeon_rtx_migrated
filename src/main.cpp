f993a501159a1674139e83ecd49414cfeea4d516
Fungeon_Bloke <qwerty123451987@hotmail.co.uk>
2024-08-24 10:33:20 +0100

    Create a simple main function + accompanying CMakeLists

---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class fungeon_rtx_hello
+{
+	public:
+	void run()
+	{
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+	
+private:
+	void initWindow() {
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Fungeon_RTX_Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan()
+	{
+
+    }
+
+    void mainLoop() {
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+    }
+
+    void cleanup()
+	{
+		glfwDestroyWindow(window);
+		glfwTerminate();
+    }
+
+	GLFWwindow* window;
+}
+
+int main(int argc, const char** argv)
+{
+	fungeon_rtx_hello app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
